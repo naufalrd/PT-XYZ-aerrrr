e86ed509be9d3e0c2b42d4f175fd33f3e4cc1003
naufalrd <naufalrafifdanutirtaaa@gmail.com>
2021-04-13 12:52:09 +0700

    change password form

--- application/controllers/Pelanggan.php
+++ application/controllers/Pelanggan.php
@@ -13,11 +13,12 @@ class Pelanggan extends CI_Controller
         }
     }
 
-    public function index(){
+    public function index()
+    {
         $data['keluhan'] = $this->pelanggan_model->get_keluhan();
         $data['riwayat'] = $this->pelanggan_model->get_riwayat();
         $this->load->view('template/header.php');
-        $this->load->view('pelanggan/home.php',$data);
+        $this->load->view('pelanggan/home.php', $data);
         $this->load->view('template/footer.php');
     }
 
@@ -28,18 +29,20 @@ class Pelanggan extends CI_Controller
         $this->load->view('template/footer.php');
     }
 
-    public function biodata($username){
-        $where=[
-            'username'=>$username
+    public function biodata($username)
+    {
+        $where = [
+            'username' => $username
         ];
         $data['pelanggan'] = $this->pelanggan_model->edit_pelanggan($where)->result();
         $this->load->view('template/header.php');
-        $this->load->view('pelanggan/biodata.php',$data);
+        $this->load->view('pelanggan/biodata.php', $data);
         $this->load->view('template/footer.php');
     }
-    
 
-    public function add_keluhan(){
+
+    public function add_keluhan()
+    {
         $this->load->helper('date');
         date_default_timezone_set('Asia/Jakarta');
         $now = date('Y-m-d');
@@ -52,66 +55,70 @@ class Pelanggan extends CI_Controller
             'tanggal_keluhan' => $now
         ];
         $table = 'keluhan';
-        $this->pelanggan_model->tambah_keluhan($table,$data);
+        $this->pelanggan_model->tambah_keluhan($table, $data);
         redirect('pelanggan');
     }
 
-    public function details($id){
+    public function details($id)
+    {
         $data['feedback'] = $this->pelanggan_model->monitor_feedback($id);
         $data['keluhan'] = $this->pelanggan_model->monitor_keluhan($id);
         $this->load->view('template/header.php');
-        $this->load->view('pelanggan/details.php',$data);
+        $this->load->view('pelanggan/details.php', $data);
         $this->load->view('template/footer.php');
     }
 
-    public function tambah_respon($id,$idkeluhan){
+    public function tambah_respon($id, $idkeluhan)
+    {
         // ini buat tabel feedback
-        $where=[
-            'id_feedback'=>$id
+        $where = [
+            'id_feedback' => $id
         ];
-        $data=[
-            'feedback'=>$this->input->post('respon_pelanggan')
+        $data = [
+            'feedback' => $this->input->post('respon_pelanggan')
         ];
-        $table='feedback';
+        $table = 'feedback';
         // ini buat tabel keluhan
-        $where2=[
-            'id_keluhan'=>$idkeluhan
+        $where2 = [
+            'id_keluhan' => $idkeluhan
         ];
-        $data2=[
-            'status_pesan'=>'pelanggan'
+        $data2 = [
+            'status_pesan' => 'pelanggan'
         ];
-        $table2='keluhan';
-        $this->pelanggan_model->update_data($where,$data,$table);
-        $this->pelanggan_model->update_data($where2,$data2,$table2);
-        redirect('pelanggan/details/'.$idkeluhan);
+        $table2 = 'keluhan';
+        $this->pelanggan_model->update_data($where, $data, $table);
+        $this->pelanggan_model->update_data($where2, $data2, $table2);
+        redirect('pelanggan/details/' . $idkeluhan);
     }
 
-    public function selesai($keluhan,$feedback){
+    public function selesai($keluhan, $feedback)
+    {
         //ini buat tabel keluhan
-        $where=[
-            'id_keluhan'=>$keluhan
+        $where = [
+            'id_keluhan' => $keluhan
         ];
-        $data=[
-            'status'=> "Selesai",
-            'rating'=> $this->input->post('rating'),
-            'rating_desc'=> $this->input->post('rating_desc'),
-            'status_pesan'=> ''
+        $data = [
+            'status' => "Selesai",
+            'rating' => $this->input->post('rating'),
+            'rating_desc' => $this->input->post('rating_desc'),
+            'status_pesan' => ''
         ];
-        $table='keluhan';
+        $table = 'keluhan';
         //ini buat tabel feedback
-        $where2=[
-            'id_feedback'=>$feedback
+        $where2 = [
+            'id_feedback' => $feedback
         ];
-        $data2=[
-            'feedback'=>"Baik, terimakasih !"
+        $data2 = [
+            'feedback' => "Baik, terimakasih !"
         ];
-        $table2='feedback';
-        $this->pelanggan_model->update_data($where,$data,$table);
-        $this->pelanggan_model->update_data($where2,$data2,$table2);
-        redirect('pelanggan/details/'.$keluhan);
+        $table2 = 'feedback';
+        $this->pelanggan_model->update_data($where, $data, $table);
+        $this->pelanggan_model->update_data($where2, $data2, $table2);
+        redirect('pelanggan/details/' . $keluhan);
     }
-  
-    public function update_biodata(){
+
+    public function update_biodata()
+    {
         $nama_depan = $this->input->post('nama_depan');
         $nama_belakang = $this->input->post('nama_belakang');
         $email_user = $this->input->post('email_user');
@@ -125,12 +132,39 @@ class Pelanggan extends CI_Controller
             'nama_belakang' => $nama_belakang,
             'alamat' => $alamat
         ];
-        $where =[
+        $where = [
             'username' => $this->input->post('username')
         ];
         $table = 'user';
-        $this->pelanggan_model->update_data($where,$data,$table);
+        $this->pelanggan_model->update_data($where, $data, $table);
         redirect('pelanggan');
     }
 
+    public function update_password()
+    {
+        $id_user = $this->input->post('id_user');
+        $password = $this->input->post('password');
+        $old_password = $this->input->post('old_password');
+        $new_password = $this->input->post('new_password');
+        $re_password = $this->input->post('re_password');
+
+        $new_password = password_hash($new_password, PASSWORD_DEFAULT);
+        if (password_verify($old_password, $password)) {
+            $data = [
+                'password' => $new_password,
+            ];
+            $where = [
+                'id_user' => $id_user
+            ];
+            $table = 'user';
+            $this->pelanggan_model->update_data($where, $data, $table);
+            redirect('pelanggan');
+        } else {
+            $data = [
+                'type' => 'error',
+                'msg' => 'maaf password yang anda masukkan salah.'
+            ];
+            echo json_encode($data);
+        }
+    }
 }
--- application/views/pelanggan/biodata.php
+++ application/views/pelanggan/biodata.php
@@ -1,35 +1,81 @@
 <div class="container mb-container">
-    <div class="row mt-5 mx-5 p-5 shadow bg-white rounded">
-        <h1 class="text-center mb-5">Edit User</h1>
-        <div class="container text-center overflow-auto">
-        <?php foreach($pelanggan as $a) :?>
-            <form action="<?= site_url(); ?>pelanggan/update_biodata" method="post">
+    <div class="row mt-5 mx-5 p-5 shadow bg-white rounded editBiodata">
+        <h1 class="text-center">Edit User</h1>
+        <div class="col text-center mb-5">
+            <a id="editPassword" class="btn btn-outline-dark btn-sm">Edit Password</a>
+        </div>
+        <div class="container text-start overflow-auto">
+            <?php foreach ($pelanggan as $a) : ?>
+                <form action="<?= site_url(); ?>pelanggan/update_biodata" method="post">
+                    <div class="row">
+
+                        <input type="hidden" name="username" value="<?= $a->username ?>">
+                        <div class="form-group col-6 mb-3">
+                            <label for="inputEmail" class="form-controll">Nama Depan</label>
+                            <input type="text" name="nama_depan" class="form-control shadow-none" placeholder="Nama Depan" value="<?= $a->nama_depan ?>" required autofocus>
+                        </div>
+                        <div class="form-group col-6 mb-3">
+                            <label for="inputEmail" class="form-controll">Nama Belakang</label>
+                            <input type="text" name="nama_belakang" class="form-control shadow-none" placeholder="Nama Belakang" value="<?= $a->nama_belakang ?>" required>
+                        </div>
+                        <div class="form-group col-6 mb-3">
+                            <label for="inputEmail" class="form-controll">No. HP</label>
+                            <input type="text" name="no_hp" class="form-control shadow-none" placeholder="No. HP" value="<?= $a->no_hp ?>" required>
+                        </div>
+                        <div class="form-group col-6 mb-3">
+                            <label for="inputEmail" class="form-controll">E-mail</label>
+                            <input type="text" name="email_user" class="form-control shadow-none" placeholder="E-mail" value="<?= $a->email_user ?>" required>
+                        </div>
+                        <div class="form-group col-12 mb-3">
+                            <label for="inputEmail" class="form-controll">Alamat</label>
+                            <textarea type="text" name="alamat" class="form-control" autocomplete="off" placeholder="alamat"> <?= $a->alamat ?> </textarea>
+                        </div>
+                    </div>
+                    <button class="btn btn-md btn-primary" type="submit">Update Biodata</button>
+                </form>
+        </div>
+    </div>
+    <div class="row mt-5 mx-5 p-5 shadow bg-white rounded editPassword d-none">
+        <h1 class="text-center">Edit Password</h1>
+        <div class="col text-center mb-5">
+            <a onclick="editBiodata()" class="btn btn-outline-dark btn-sm">Edit Biodata</a>
+        </div>
+        <div class="container text-start overflow-auto">
+            <form action="<?= site_url(); ?>pelanggan/update_password" method="post">
                 <div class="row">
-                    <input type="hidden" name="username" value="<?=$a->username?>">                  
-                    <div class="form-group col-6 mb-3">
-                        <label for="inputEmail" class="form-controll">Nama Depan</label>
-                        <input type="text" name="nama_depan" class="form-control shadow-none" placeholder="Nama Depan" value="<?=$a->nama_depan?>" required autofocus>
-                    </div>
-                    <div class="form-group col-6 mb-3">
-                        <label for="inputEmail" class="form-controll">Nama Belakang</label>
-                        <input type="text" name="nama_belakang" class="form-control shadow-none" placeholder="Nama Belakang" value="<?=$a->nama_belakang?>" required>
-                    </div>
-                    <div class="form-group col-6 mb-3">
-                        <label for="inputEmail" class="form-controll">No. HP</label>
-                        <input type="text" name="no_hp" class="form-control shadow-none" placeholder="No. HP" value="<?=$a->no_hp?>" required>
-                    </div>
-                    <div class="form-group col-6 mb-3">
-                        <label for="inputEmail" class="form-controll">E-mail</label>
-                        <input type="text" name="email_user" class="form-control shadow-none" placeholder="E-mail" value="<?=$a->email_user?>" required>
+                    <input type="hidden" name="id_user" value="<?= $this->session->userdata('id_user'); ?>">
+                    <input type="hidden" name="password" value="<?= $a->password; ?>">
+                    <div class="form-group col-12 mb-3">
+                        <label for="inputEmail" class="form-controll">Password Lama</label>
+                        <input type="password" name="old_password" class="form-control shadow-none" placeholder="Nama Depan" required autofocus>
                     </div>
                     <div class="form-group col-12 mb-3">
-                        <label for="inputEmail" class="form-controll">Alamat</label>
-                        <textarea type="text" name="alamat" class="form-control" autocomplete="off" placeholder="alamat"> <?=$a->alamat?> </textarea>
+                        <label for="inputEmail" class="form-controll">Password Baru</label>
+                        <input type="password" name="new_password" class="form-control shadow-none" placeholder="Nama Belakang" required>
+                    </div>
+                    <div class="form-group col-12 mb-3">
+                        <label for="inputEmail" class="form-controll">Retype Password Baru</label>
+                        <input type="password" name="re_password" class="form-control shadow-none" placeholder="No. HP" required>
                     </div>
                 </div>
-                <button class="btn btn-lg btn-primary" type="submit">Update Biodata</button>
+                <div class="text-center col">
+                    <button class="btn btn-md btn-primary" type="submit">Update Password</button>
+                </div>
             </form>
-            <?php endforeach ?>
+
+        <?php endforeach ?>
         </div>
     </div>
 </div>
+<script>
+    document.getElementById('editPassword').addEventListener('click', () => {
+        document.querySelector('.editBiodata').className = 'row mt-5 mx-5 p-5 shadow bg-white rounded editBiodata d-none'
+        document.querySelector('.editPassword').className = 'row mt-5 mx-5 p-5 shadow bg-white rounded editPassword'
+    })
+
+    async function editPassword() {
+        document.querySelector('.editBiodata').className = 'row mt-5 mx-5 p-5 shadow bg-white rounded editBiodata'
+        document.querySelector('.editPassword').className = 'row mt-5 mx-5 p-5 shadow bg-white rounded editPassword d-none'
+    }
+
+</script>
