4dc369f17a5cf7549c0a0ae28cdb426ce5f9937e
yesyualeon <yesyualeon@gmail.com>
2021-05-28 17:29:13 +0700

    Director details 4

--- application/models/Direktur_model.php
+++ application/models/Direktur_model.php
@@ -37,9 +37,9 @@ class Direktur_model extends CI_Model
     public function monitor_feedback($id)
     {
         $this->db->select('*');
-        $this->db->from('feedback');
+		$this->db->from('feedback');
         $this->db->join('keluhan', 'feedback.id_keluhan = keluhan.id_keluhan');
-        $this->db->join('user', 'user.id_user = keluhan.id_user');
+		$this->db->join('user', 'user.id_user = keluhan.id_user');
         $this->db->join('bidang', 'bidang.id_bidang = keluhan.id_bidang');
         $this->db->where('keluhan.id_keluhan', $id);
         $this->db->order_by('feedback.id_feedback', 'asc');
